dee3f8569ec512d288e32c12496c69b7a8d114e0
sabina929 <sabina.abbasova2909@gmail.com>
2019-08-31 17:25:02 +0400

    some changes

--- script.js
+++ script.js
@@ -1,78 +1,68 @@
-const container = document.querySelector('#period');
-const selection = document.querySelector('#period p span');
-const optionOne = document.querySelector('.options p:nth-child(1)');
-const optionTwo = document.querySelector('.options p:nth-child(2)');
-const optionThree = document.querySelector('.options p:nth-child(3)');
-const optionFour = document.querySelector('.options p:nth-child(4)');
+const containers = document.querySelectorAll('.period');
+const selections = document.querySelectorAll('.period>p>span');
+const firstOptions = document.querySelectorAll('.options p:nth-child(1)');
+const secondOptions = document.querySelectorAll('.options p:nth-child(2)');
+const thirdOptions = document.querySelectorAll('.options p:nth-child(3)');
+const fourthtOptions = document.querySelectorAll('.options p:nth-child(4)');
 
-// console.log(selection);
-// console.log(optionOne);
-// console.log(optionTwo);
-// console.log(optionThree);
-// console.log(optionFour);
+// console.log(containers);
+// console.log(selections);
+// console.log(firstOptions);
+// console.log(secondOptions);
+// console.log(thirdOptions);
+// console.log(fourthtOptions);
 
-container.addEventListener('click', expandContainer);
+containers.forEach(container => {
+    container.addEventListener('click', () => {
+        container.classList.toggle('expand');
+    });
+});
 
-optionOne.addEventListener('click', changeSelectionToOptionOne);
-optionTwo.addEventListener('click', changeSelectionToOptionTwo);
-optionThree.addEventListener('click', changeSelectionToOptionThree);
-optionFour.addEventListener('click', changeSelectionToOptionFour);
 
-function expandContainer() {
-    container.classList.toggle('expand');
-    // console.log('clicked');
+firstOptions.forEach(firstOption => {
+    firstOption.addEventListener('click', changeSelectionToFirstOption);
+});
+
+secondOptions.forEach(firstOption => {
+    firstOption.addEventListener('click', changeSelectionToSecondOption);
+});
+
+thirdOptions.forEach(firstOption => {
+    firstOption.addEventListener('click', changeSelectionToThirdOption);
+});
+
+fourthtOptions.forEach(firstOption => {
+    firstOption.addEventListener('click', changeSelectionToFourthtOption);
+});
+
+
+
+function changeSelectionToFirstOption(e) {
+    // console.log(e.target);
+    // console.log(e.target.parentNode.parentNode.previousSibling.previousSibling.previousSibling.previousSibling.childNodes[1]);
+
+    let selected = e.target.parentNode.parentNode.previousSibling.previousSibling.previousSibling.previousSibling.childNodes[1];
+    selected.innerHTML = 'All time';
+
 }
 
-function changeSelectionToOptionOne() {
-    selection.innerHTML = 'All time';
+function changeSelectionToSecondOption(e) {
+
+    let selected = e.target.parentNode.parentNode.previousSibling.previousSibling.previousSibling.previousSibling.childNodes[1];
+    selected.innerHTML = 'Last year';
+
 }
 
-function changeSelectionToOptionTwo() {
-    selection.innerHTML = 'Last year';
+function changeSelectionToThirdOption(e) {
+
+    let selected = e.target.parentNode.parentNode.previousSibling.previousSibling.previousSibling.previousSibling.childNodes[1];
+    selected.innerHTML = 'Last month';
+
 }
 
-function changeSelectionToOptionThree() {
-    selection.innerHTML = 'Last month';
-}
+function changeSelectionToFourthtOption(e) {
 
-function changeSelectionToOptionFour() {
-    selection.innerHTML = 'Last week';
-}
+    let selected = e.target.parentNode.parentNode.previousSibling.previousSibling.previousSibling.previousSibling.childNodes[1];
+    selected.innerHTML = 'Last week';
 
-// SELECTION2
-
-const container2 = document.querySelector('#period2');
-const selection2 = document.querySelector('#period2 p span');
-const optionOne2 = document.querySelector('#period2 .options2 p:nth-child(1)');
-const optionTwo2 = document.querySelector('#period2 .options2 p:nth-child(2)');
-const optionThree2 = document.querySelector('#period2 .options2 p:nth-child(3)');
-const optionFour2 = document.querySelector('#period2 .options2 p:nth-child(4)');
-
-
-
-container2.addEventListener('click', expandContainer2);
-
-optionOne2.addEventListener('click', changeSelection2ToOptionOne);
-optionTwo2.addEventListener('click', changeSelection2ToOptionTwo);
-optionThree2.addEventListener('click', changeSelection2ToOptionThree);
-optionFour2.addEventListener('click', changeSelection2ToOptionFour);
-
-function expandContainer2() {
-    container2.classList.toggle('expand2');
-}
-
-function changeSelection2ToOptionOne() {
-    selection2.innerHTML = 'All time';
-}
-
-function changeSelection2ToOptionTwo() {
-    selection2.innerHTML = 'Last year';
-}
-
-function changeSelection2ToOptionThree() {
-    selection2.innerHTML = 'Last month';
-}
-
-function changeSelection2ToOptionFour() {
-    selection2.innerHTML = 'Last week';
 }
